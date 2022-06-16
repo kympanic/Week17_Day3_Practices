b74c8856f047aa74dd3fb799a6b1121283096416
Daniel Yoo <dacapo0509@gmail.com>
2022-06-15 20:32:29 -0700

    practice complete for element selection

--- practice-for-week-09-element-selection-main/selectors.js
+++ practice-for-week-09-element-selection-main/selectors.js
@@ -1,38 +1,69 @@
 const select = () => {
-    /* Write queries for each of the following */
+	/* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here
-    
-    // 2. Get all seedless fruit elements
-    // Your code here
+	/* Section 1 */
+	// 1. Get all seeded fruit elements
+	const seededFruit = document.querySelectorAll(".seed");
+	// console.log(seededFruit);
 
-    // 3. Get first seedless fruit element
-    // Your code here
+	// 2. Get all seedless fruit elements
+	const seedlessFruit = document.querySelectorAll(".seedless");
+	// console.log(seedlessFruit);
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here
+	// 3. Get first seedless fruit element
+	const firstSeedless = document.querySelector(".seedless");
+	// console.log(firstSeedless);
 
-    // 5. Get all children of element "wrapper"
-    // Your code here
+	/* Section 2 */
+	// 4. Get inner span with text "you"
+	const innerSpan = document.querySelector("span");
+	console.log(innerSpan);
 
-    // 6. Get all odd number list items in the list
-    // Your code here
+	// 5. Get all children of element "wrapper"
+	const wrapperChildren = document.querySelector("#wrapper").children;
 
-    // 7. Get all even number list items in the list
-    // Your code here
+	console.log(wrapperChildren);
 
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here
+	// 6. Get all odd number list items in the list
+	const li = [...document.querySelectorAll("li")];
 
-    // 9. Get "Amazon" list element
-    // Your code here
+	oddList = li.filter((element, index) => {
+		if (index % 2 !== 0) {
+			return true;
+		}
+	});
+	// console.log(oddList);
 
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here
-}
+	// 7. Get all even number list items in the list
+	const li2 = [...document.querySelectorAll("li")];
+
+	evenList = li2.filter((element, index) => {
+		if (index % 2 === 0) {
+			return true;
+		}
+	});
+	console.log(evenList);
+
+	/* Section 3 */
+	// 8. Get all tech companies without a class name
+	const anchors = [...document.querySelectorAll("a")];
+	const innerTest = anchors.map((el) => {
+		return el.innerText;
+	});
+	console.log(innerTest);
+	// 9. Get "Amazon" list element
+	const amazon = anchors.reduce((prevValue, currentValue) => {
+		if (currentValue.innerText === "Amazon") return currentValue;
+	}, "");
+
+	console.log(amazon);
+
+	// 10. Get all unicorn list elements (not the image element)
+	const unicornClass = [...document.querySelectorAll(".unicorn")];
+	let unicornParents = unicornClass.map((ele) => {
+		return ele.parentElement;
+	});
+	console.log(unicornParents);
+};
 
 window.onload = select;
